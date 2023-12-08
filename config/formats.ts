dc9929997661622a053d9aaadccc698b202a792e
Cody Spratford <koreanpanda345@gmail.com>
2023-12-08 12:48:55 -0800

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -18,146 +18,152 @@ The column value will be ignored for repeat sections.
 */
 
 export const Formats: FormatList = [
-
 	{
-		section: "The Trainer Corner's Draft",
+		section: "Testing",
 	},
 	{
-		name: "[Gen 9] National Dex Draft",
-		desc: "The Official Draft Format for TTC",
-		mod: 'test',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'No Shell Smash Mega Toise', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause', 'No Calm Mind For The Patio Set'],
-		banlist: [
-			'ND Uber', 'ND AG', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
-			'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
-		],
-		// Already Allowed:
-		/**
-		 * Lutrous Globe
-		 * Bleakwind Storm
-		 * Lunar Blessing
-		 * Mystical Power
-		 * Sandsear Storm
-		 * Wildbolt Storm
-		 */
-		unbanlist: [
-			'Unreleased',
-			// Unobtainable Moves
-			'Barb Barrage',
-			'Blazing Torque',
-			'Ceaseless Edge',
-			'Chloroblast',
-			'Combat Torque',
-			'Dire Claw',
-			"Dragon's Maw",
-			'Esper Wing',
-			'Infernal Parade',
-			'Magical Torque',
-			'Mountain Gale',
-			'Noxious Torque',
-			'Psyshield Bash',
-			'Raging Fury',
-			'Shelter',
-			'Springtide Storm',
-			'Stone Axe',
-			'Triple Arrows',
-			'Victory Dance',
-			'Wicked Torque',
-			'Custap Berry',
-			// LGPE
-			'LGPE',
-			'Baton Pass',
-		],
-	},
-	{
-		section: "Other Draft Leagues",
-	},
-	{
-		name: "[Gen 9] Other National Dex Draft",
-		desc: "This version of the National Dex Draft is for other leagues to use. Since TTC NatDex Draft format is a bit more stricter, it might be flexible with other leagues.",
-		mod: "ttc_current",
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause'],
-		banlist: [
-			'ND Uber', 'ND AG', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
-			'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
-		],
-		unbanlist: [
-			'Unreleased',
-			// Unobtainable Moves
-			'Barb Barrage',
-			'Blazing Torque',
-			'Ceaseless Edge',
-			'Chloroblast',
-			'Combat Torque',
-			'Dire Claw',
-			"Dragon's Maw",
-			'Esper Wing',
-			'Infernal Parade',
-			'Magical Torque',
-			'Mountain Gale',
-			'Noxious Torque',
-			'Psyshield Bash',
-			'Raging Fury',
-			'Shelter',
-			'Springtide Storm',
-			'Stone Axe',
-			'Triple Arrows',
-			'Victory Dance',
-			'Wicked Torque',
-			'Custap Berry',
-			// LGPE
-			'LGPE',
-			'Baton Pass',
-		],
-	},
-	{
-		section: "Public Test Realm",
-	},
-	{
-		name: "[Gen 9] National Dex PTR",
-		desc: "The Official Draft Format for TTC",
-		mod: 'ttc_current',
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'No Shell Smash Mega Toise', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause', 'No Calm Mind For The Patio Set'],
-		banlist: [
-			'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
-			'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
-		],
-		// Already Allowed:
-		/**
-		 * Lutrous Globe
-		 * Bleakwind Storm
-		 * Lunar Blessing
-		 * Mystical Power
-		 * Sandsear Storm
-		 * Wildbolt Storm
-		 */
-		unbanlist: [
-			'Unreleased',
-			// Unobtainable Moves
-			'Barb Barrage',
-			'Blazing Torque',
-			'Ceaseless Edge',
-			'Chloroblast',
-			'Combat Torque',
-			'Dire Claw',
-			"Dragon's Maw",
-			'Esper Wing',
-			'Infernal Parade',
-			'Magical Torque',
-			'Mountain Gale',
-			'Noxious Torque',
-			'Psyshield Bash',
-			'Raging Fury',
-			'Shelter',
-			'Springtide Storm',
-			'Stone Axe',
-			'Triple Arrows',
-			'Victory Dance',
-			'Wicked Torque',
-			'Custap Berry',
-			// LGPE
-			'LGPE',
-			'Baton Pass',
-		],
-	},
+		name: "[Gen 9] Testing",
+		mod: "test",
+	}
+	// {
+	// 	section: "The Trainer Corner's Draft",
+	// },
+	// {
+	// 	name: "[Gen 9] National Dex Draft",
+	// 	desc: "The Official Draft Format for TTC",
+	// 	mod: 'test',
+	// 	ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'No Shell Smash Mega Toise', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause', 'No Calm Mind For The Patio Set'],
+	// 	banlist: [
+	// 		'ND Uber', 'ND AG', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+	// 		'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
+	// 	],
+	// 	// Already Allowed:
+	// 	/**
+	// 	 * Lutrous Globe
+	// 	 * Bleakwind Storm
+	// 	 * Lunar Blessing
+	// 	 * Mystical Power
+	// 	 * Sandsear Storm
+	// 	 * Wildbolt Storm
+	// 	 */
+	// 	unbanlist: [
+	// 		'Unreleased',
+	// 		// Unobtainable Moves
+	// 		'Barb Barrage',
+	// 		'Blazing Torque',
+	// 		'Ceaseless Edge',
+	// 		'Chloroblast',
+	// 		'Combat Torque',
+	// 		'Dire Claw',
+	// 		"Dragon's Maw",
+	// 		'Esper Wing',
+	// 		'Infernal Parade',
+	// 		'Magical Torque',
+	// 		'Mountain Gale',
+	// 		'Noxious Torque',
+	// 		'Psyshield Bash',
+	// 		'Raging Fury',
+	// 		'Shelter',
+	// 		'Springtide Storm',
+	// 		'Stone Axe',
+	// 		'Triple Arrows',
+	// 		'Victory Dance',
+	// 		'Wicked Torque',
+	// 		'Custap Berry',
+	// 		// LGPE
+	// 		'LGPE',
+	// 		'Baton Pass',
+	// 	],
+	// },
+	// {
+	// 	section: "Other Draft Leagues",
+	// },
+	// {
+	// 	name: "[Gen 9] Other National Dex Draft",
+	// 	desc: "This version of the National Dex Draft is for other leagues to use. Since TTC NatDex Draft format is a bit more stricter, it might be flexible with other leagues.",
+	// 	mod: "ttc_current",
+	// 	ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause'],
+	// 	banlist: [
+	// 		'ND Uber', 'ND AG', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+	// 		'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
+	// 	],
+	// 	unbanlist: [
+	// 		'Unreleased',
+	// 		// Unobtainable Moves
+	// 		'Barb Barrage',
+	// 		'Blazing Torque',
+	// 		'Ceaseless Edge',
+	// 		'Chloroblast',
+	// 		'Combat Torque',
+	// 		'Dire Claw',
+	// 		"Dragon's Maw",
+	// 		'Esper Wing',
+	// 		'Infernal Parade',
+	// 		'Magical Torque',
+	// 		'Mountain Gale',
+	// 		'Noxious Torque',
+	// 		'Psyshield Bash',
+	// 		'Raging Fury',
+	// 		'Shelter',
+	// 		'Springtide Storm',
+	// 		'Stone Axe',
+	// 		'Triple Arrows',
+	// 		'Victory Dance',
+	// 		'Wicked Torque',
+	// 		'Custap Berry',
+	// 		// LGPE
+	// 		'LGPE',
+	// 		'Baton Pass',
+	// 	],
+	// },
+	// {
+	// 	section: "Public Test Realm",
+	// },
+	// {
+	// 	name: "[Gen 9] National Dex PTR",
+	// 	desc: "The Official Draft Format for TTC",
+	// 	mod: 'ttc_current',
+	// 	ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod', 'No Shell Smash Mega Toise', 'Z-Move Clause', 'Dynamax Clause', 'Terastal Clause', 'No Calm Mind For The Patio Set'],
+	// 	banlist: [
+	// 		'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+	// 		'Quick Claw', 'Razor Fang', 'Assist', 'Last Respects',
+	// 	],
+	// 	// Already Allowed:
+	// 	/**
+	// 	 * Lutrous Globe
+	// 	 * Bleakwind Storm
+	// 	 * Lunar Blessing
+	// 	 * Mystical Power
+	// 	 * Sandsear Storm
+	// 	 * Wildbolt Storm
+	// 	 */
+	// 	unbanlist: [
+	// 		'Unreleased',
+	// 		// Unobtainable Moves
+	// 		'Barb Barrage',
+	// 		'Blazing Torque',
+	// 		'Ceaseless Edge',
+	// 		'Chloroblast',
+	// 		'Combat Torque',
+	// 		'Dire Claw',
+	// 		"Dragon's Maw",
+	// 		'Esper Wing',
+	// 		'Infernal Parade',
+	// 		'Magical Torque',
+	// 		'Mountain Gale',
+	// 		'Noxious Torque',
+	// 		'Psyshield Bash',
+	// 		'Raging Fury',
+	// 		'Shelter',
+	// 		'Springtide Storm',
+	// 		'Stone Axe',
+	// 		'Triple Arrows',
+	// 		'Victory Dance',
+	// 		'Wicked Torque',
+	// 		'Custap Berry',
+	// 		// LGPE
+	// 		'LGPE',
+	// 		'Baton Pass',
+	// 	],
+	// },
 ];
